46d95d34c1d18722862182607eb7d21c637b5ed8
André Nespolon <andre.nespolon@gmail.com>
2021-08-13 10:55:44 -0300

    includes the problem description

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# c++ code training of code forces
+# c++ code training of [codeforces](https://codeforces.com/)
 
 Simple codes developed in Code Forces contests. See more in my [profile](https://codeforces.com/profile/andre.nesp).
--- watermelon/main.cpp
+++ watermelon/main.cpp
@@ -1,3 +1,10 @@
+/*
+One hot summer day Pete and his friend Billy decided to buy a watermelon. They 
+chose the biggest and the ripest one, in their opinion. After that the 
+watermelon was weighed, and the scales showed w kilos. They rushed home, dying 
+of thirst, and decided to divide the berry, however they faced a hard problem.
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
